0de775bfcc8c8680844ead0cbaa153949955dac4
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 20:43:29 +0300

    question 19

--- assigmentt 03 c# basics/Program.cs
+++ assigmentt 03 c# basics/Program.cs
@@ -209,6 +209,18 @@ namespace assigmentt_03_c__basics
 
             //    Console.WriteLine(book);
             //}
+
+            //19. Using the same books array, print every title except "The Pragmatic Programmer" (skip it with continue, don't stop the loop)
+            //foreach (string book in books)
+            //{
+            //    if (book == "The Pragmatic Programmer")
+            //    {
+            //        continue;
+            //    }
+
+            //    Console.WriteLine(book);
+            //}
+
         }
 
     }
