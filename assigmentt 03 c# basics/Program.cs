2801d08d2add71c0a62fbd7f7819e27d67b9bacd
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 20:34:18 +0300

    question 12

--- assigmentt 03 c# basics/Program.cs
+++ assigmentt 03 c# basics/Program.cs
@@ -135,6 +135,23 @@ namespace assigmentt_03_c__basics
             //    Console.WriteLine("You can borrow this book");
             //}
 
+            //12. Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+            //string title = "Refactoring";
+
+            //switch (title)
+            //{
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice!");
+            //        break;
+
+            //    case "Refactoring":
+            //        Console.WriteLine("Nice pick!");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Never heard of it");
+            //        break;
+            //}
         }
     }
 }
