b0ebc19b86c65f313e3c23aafc1c91d11c61eded
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 20:37:38 +0300

    question 14

--- assigmentt 03 c# basics/Program.cs
+++ assigmentt 03 c# basics/Program.cs
@@ -160,6 +160,19 @@ namespace assigmentt_03_c__basics
 
             //Console.WriteLine(sizeLabel);
 
+            //14.  Given string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };, use a for loop to print each book with its position number, like 1.Clean Code.
+//            string[] books =
+//{
+//    "Clean Code",
+//    "The Pragmatic Programmer",
+//    "Refactoring"
+//};
+
+//            for (int i = 0; i < books.Length; i++)
+//            {
+//                Console.WriteLine($"{i + 1}. {books[i]}");
+//            }
+
         }
 
     }
