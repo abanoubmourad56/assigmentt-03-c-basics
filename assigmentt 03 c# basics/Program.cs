90bd310642aede19b350eea93fdf7cc1d125c5b8
abanoubmourad99-crypt <abanoubmourad99@gmail.com>
2026-08-25 19:41:26 +0300

    question 1

--- assigmentt 03 c# basics/Program.cs
+++ assigmentt 03 c# basics/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+            //1. Create Book class, object, store it in object, then print
+            //    public class Book
+            //{
+            //    public string Title { get; set; }
+            //    public int Pages { get; set; }
+            //}
 
+            //Book book = new Book
+            //{
+            //    Title = "Clean Code",
+            //    Pages = 464
+            //};
+
+            //object obj = book;
+
+            //Console.WriteLine(obj);
         }
     }
 }
